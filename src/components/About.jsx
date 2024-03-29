632994f1fb58b985609bd38c27a8e75f25afe8b5
mhammadfarooq20 <muhammad.hammad.farooq.02@gmail.com>
2024-03-29 11:08:15 +0500

    add About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,35 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
-// import Tilt from "react-tilt";
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-// import { services } from "../constants";
+import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+
+          }}
+          >
+            
+          </div>
+
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
@@ -16,7 +41,7 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         I'm a skilled software developer with experience in TypeScript and
         Python, and expertise in frameworks like Next.js,React, Node.js, and
@@ -24,6 +49,11 @@ const About = () => {
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
