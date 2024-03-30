f79424c2ee8ea484e378820809dd8a8aab068bb4
mhammadfarooq20 <muhammad.hammad.farooq.02@gmail.com>
2024-03-30 17:48:38 +0500

    Complete about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,17 @@
-/* eslint-disable react/no-unknown-property */
-/* eslint-disable react/prop-types */
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable no-unused-vars */
-import Tilt from "react-tilt";
+import React from "react";
+import { Tilt } from "react-tilt";
+
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
@@ -20,12 +19,14 @@ const ServiceCard = ({ index, title, icon }) => {
             max: 45,
             scale: 1,
             speed: 450,
-
           }}
-          >
-            
-          </div>
-
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt="title" className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
       </motion.div>
     </Tilt>
   );
@@ -38,16 +39,16 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
-
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
-        Python, and expertise in frameworks like Next.js,React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I&apos;m a skilled software developer with experience in TypeScript and
+        Python, and expertise in frameworks like Next.Js, React, Node.js, and
+        Three.js. I&apos;m a quick learner and collaborate closely with clients
+        to create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let&apos;s work together to bring your ideas to
+        life!
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
@@ -58,4 +59,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
