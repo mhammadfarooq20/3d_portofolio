7228e26aa0e5dfcea683d787c299ed8d82507a0f
mhammadfarooq20 <muhammad.hammad.farooq.02@gmail.com>
2024-06-07 19:01:44 +0500

    Feedback complete

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -14,11 +16,26 @@ const FeedbackCard = ({
   image,
 }) => (
   <motion.div
-  variants={fadeIn("", "spring", index * 0.5, 0.75)}
-  className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
-
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
   >
-    <p> {testimonial} </p>
+    <p className=" text-white font-black text-[48px]">&apos;&apos;</p>
+    <div className="mt-1">
+      <p className="text-white tracking-wider text-[18px]" > {testimonial} </p>
+      <div className="mt-7 flex justify-between items-center gap-1">
+        <div className="flex flex-1 flex-col">
+          <p className="text-white font-medium text-[16px]" >
+            <span className="blue-text-gradient" >@</span> {name}
+          </p>
+          <p className="mt-1 text-secondary text-[12px]" >{designation} of {company}</p>
+        </div>
+        <img
+          src={image}
+          alt={`feedback_by-${name}`}
+          className="w-10 h-10 rounded-full object-cover"
+        />
+      </div>
+    </div>
   </motion.div>
 );
 
@@ -46,4 +63,4 @@ const Feedbacks = () => {
   );
 };
 
-export default Feedbacks;
+export default SectionWrapper(Feedbacks, "");
